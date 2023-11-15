73818542f3250295d35f99483c7c1732ac200881
peristrophe <peristrophe@live.jp>
2023-11-15 10:54:48 +0900

    tweak lib

--- libs/utils/job_context.py
+++ libs/utils/job_context.py
@@ -8,25 +8,20 @@ from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from pyspark.sql.session import SparkSession
 
-from utils.dynamicframe import DynamicFrameReaderWriter
-
 
 @contextmanager
-def glue_job_tx() -> Generator[tuple[logging.Logger, DynamicFrameReaderWriter, Job], None, None]:
-    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-    job_name = args['JOB_NAME']
+def glue_job_tx() -> Generator[tuple[logging.Logger, GlueContext, Job], None, None]:
+    args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+    job_name = args["JOB_NAME"]
 
-    spark = SparkSession.builder.config(
-        'spark.serializer', 'org.apache.spark.serializer.KryoSerializer').getOrCreate()
-    glue_context = GlueContext(spark.sparkContext)
-    job = Job(glue_context)
+    spark = SparkSession.builder.config("spark.serializer", "org.apache.spark.serializer.KryoSerializer").getOrCreate()
+    context = GlueContext(spark.sparkContext)
+    job = Job(context)
     job.init(job_name, args)
-
-    loader = DynamicFrameReaderWriter(glue_context)
 
     logger = logging.getLogger()
     logger.info('Start glue job')
-    yield logger, loader, job
+    yield logger, context, job
 
     job.commit()
     logger.info("Finish glue job")
